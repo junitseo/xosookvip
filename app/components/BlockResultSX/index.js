396e36a6bee983d3cc32b1eb7c526a63f150b002
khangpham <khang.pham@techupcorp.com>
2023-10-25 22:06:12 +0700

    update UI , component chung

---
+++ app/components/BlockResultSX/index.js
@@ -0,0 +1,301 @@
+import React from 'react'
+import stylesCss from './styles.module.css'
+function BlockResultSX() {
+  return (
+    <div className={stylesCss['wrapper']}>
+      <div className={stylesCss['head']}>xsmn thứ 3, xsmn ngày 24/10/2023</div>
+      <div className="content">
+        <table
+          width="100%"
+          cellSpacing={0}
+          cellPadding={0}
+          border={0}
+          className="bkqmiennam"
+        >
+          <tbody>
+            <tr>
+              <td width={100} valign="top">
+                <table
+                  width="100%"
+                  cellSpacing={0}
+                  cellPadding={0}
+                  border={0}
+                  className={stylesCss['leftcl']}
+                >
+                  <tbody>
+                    <tr>
+                      <td className={stylesCss['rank']}></td>
+                    </tr>
+                    <tr>
+                      <td className={stylesCss['day']}>
+                        <a
+                          className="date"
+                        >
+                          24/10/2023
+                        </a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className={stylesCss['giai8']}> Giải tám</td>
+                    </tr>
+                    <tr>
+                      <td className={stylesCss['giai7']}> Giải bảy</td>
+                    </tr>
+                    <tr>
+                      <td className={stylesCss['giai6']}> Giải sáu</td>
+                    </tr>
+                    <tr>
+                      <td className={stylesCss['giai5']}> Giải năm</td>
+                    </tr>
+                    <tr>
+                      <td className={stylesCss['giai4']}> Giải tư</td>
+                    </tr>
+                    <tr>
+                      <td className={stylesCss['giai3']}> Giải ba</td>
+                    </tr>
+                    <tr>
+                      <td className={stylesCss['giai2']}> Giải nhì</td>
+                    </tr>
+                    <tr>
+                      <td className={stylesCss['giai1']}> Giải nhất</td>
+                    </tr>
+                    <tr>
+                      <td className={stylesCss['giaidb']}>
+                        <a href="index.html"> Giải Đặc Biệt</a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+              <td valign="top">
+                <table width="100%" cellSpacing={0} cellPadding={0} border={0}>
+                  <tbody>
+                    <tr>
+                      <td width="33.333333333333336%" valign="top">
+                        <table
+                          width="100%"
+                          cellSpacing={0}
+                          cellPadding={0}
+                          className={stylesCss['rightcl']}
+                        >
+                          <tbody>
+                            <tr>
+                              <td className={stylesCss['tinh']}>Bến Tre </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['matinh']}>XSBTR </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai8']}>
+                                <div>37</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai7']}>
+                                <div>452</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai6']}>
+                                <div>1789</div>
+                                <div>4063</div>
+                                <div>2084</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai5']}>
+                                <div>0783</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai4']}>
+                                <div>59695</div>
+                                <div>90832</div>
+                                <div>88235</div>
+                                <div>46076</div>
+                                <div>50522</div>
+                                <div>46677</div>
+                                <div>73029</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai3']}>
+                                <div>33221</div>
+                                <div>97624</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai2']}>
+                                <div>57896</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai1']}>
+                                <div>71783</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giaidb']}>
+                                <div>645938</div>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                      <td width="33.333333333333336%" valign="top">
+                        <table
+                          width="100%"
+                          cellSpacing={0}
+                          cellPadding={0}
+                          className={stylesCss['rightcl']}
+                        >
+                          <tbody>
+                            <tr>
+                              <td className={stylesCss['tinh']}>Vũng Tàu </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['matinh']}>XSVT </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai8']}>
+                                <div>10</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai7']}>
+                                <div>678</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai6']}>
+                                <div>4796</div>
+                                <div>4478</div>
+                                <div>3995</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai5']}>
+                                <div>2547</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai4']}>
+                                <div>19688</div>
+                                <div>05164</div>
+                                <div>96339</div>
+                                <div>52057</div>
+                                <div>03823</div>
+                                <div>35166</div>
+                                <div>54695</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai3']}>
+                                <div>01698</div>
+                                <div>52493</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai2']}>
+                                <div>05400</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai1']}>
+                                <div>80395</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giaidb']}>
+                                <div>625649</div>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                      <td width="33.333333333333336%" valign="top">
+                        <table
+                          width="100%"
+                          cellSpacing={0}
+                          cellPadding={0}
+                          className={stylesCss['rightcl']}
+                        >
+                          <tbody>
+                            <tr>
+                              <td className={stylesCss['tinh']}>Bạc Liêu </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['matinh']}>XSBL </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai8']}>
+                                <div>73</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai7']}>
+                                <div>732</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai6']}>
+                                <div>7968</div>
+                                <div>1541</div>
+                                <div>7028</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai5']}>
+                                <div>5850</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai4']}>
+                                <div>84216</div>
+                                <div>63215</div>
+                                <div>50263</div>
+                                <div>46576</div>
+                                <div>65901</div>
+                                <div>71032</div>
+                                <div>17440</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai3']}>
+                                <div>54807</div>
+                                <div>86004</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai2']}>
+                                <div>22202</div>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className={stylesCss['giai1']}>
+                                <div>91122</div>
+                              </td>
+                            </tr>
+                            <tr>
+                          <td className={stylesCss['giaidb']}>
+                            <div>477461</div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+    </div>
+   
+  
+  )
+}
+
+export default BlockResultSX
