e8ce81be3255a79d8d43788c941cd265177c3691
khangpham <khang.pham@techupcorp.com>
2023-11-06 15:44:28 +0700

    update Ui

--- app/components/BlockResultSX/index.js
+++ app/components/BlockResultSX/index.js
@@ -98,7 +98,7 @@ function BlockResultSX({title, data}) {
                                 <td className={stylesCss['giai8']}> Giải tám</td>
                                 {dataKq.map(item => (
                                    <td key={item.provinceName + "8"} className={stylesCss['giai8']}>
-                                   {item[9].map(i => (
+                                   {item[9]?.map(i => (
                                      <div key={i.number}>{i.number}</div>
                                    ))}
                                  </td>
@@ -109,7 +109,7 @@ function BlockResultSX({title, data}) {
                                 <td className={stylesCss['giai7']}> Giải bảy</td>
                                   {dataKq.map(item => (
                                    <td key={item.provinceName + "7"} className={stylesCss['giai7']}>
-                                   {item[8].map(i => (
+                                   {item[8]?.map(i => (
                                      <div key={i.number}>{i.number}</div>
                                    ))}
                                  </td>
@@ -119,7 +119,7 @@ function BlockResultSX({title, data}) {
                                   <td className={stylesCss['giai6']}> Giải sáu</td>
                                     {dataKq.map(item => (
                                     <td key={item.provinceName + "6"} className={stylesCss['giai6']}>
-                                    {item[7].map(i => (
+                                    {item[7]?.map(i => (
                                       <div key={i.number}>{i.number}</div>
                                     ))}
                                  </td>
@@ -129,7 +129,7 @@ function BlockResultSX({title, data}) {
                                 <td className={stylesCss['giai5']}> Giải năm</td>
                                     {dataKq.map(item => (
                                     <td key={item.provinceName + "5"} className={stylesCss['giai5']}>
-                                    {item[6].map(i => (
+                                    {item[6]?.map(i => (
                                       <div key={i.number}>{i.number}</div>
                                     ))}
                                  </td>
@@ -139,7 +139,7 @@ function BlockResultSX({title, data}) {
                                   <td className={stylesCss['giai4']}> Giải bốn</td>
                                     {dataKq.map(item => (
                                     <td key={item.provinceName + "4"} className={stylesCss['giai4']}>
-                                    {item[5].map(i => (
+                                    {item[5]?.map(i => (
                                       <div key={i.number}>{i.number}</div>
                                     ))}
                                   </td>
@@ -149,7 +149,7 @@ function BlockResultSX({title, data}) {
                                 <td className={stylesCss['giai3']}> Giải ba</td>
                                   {dataKq.map(item => (
                                    <td key={item.provinceName + "3"} className={stylesCss['giai3']}>
-                                   {item[4].map(i => (
+                                   {item[4]?.map(i => (
                                      <div key={i.number}>{i.number}</div>
                                    ))}
                                  </td>
@@ -159,7 +159,7 @@ function BlockResultSX({title, data}) {
                                   <td className={stylesCss['giai2']}>Giải hai</td>
                                     {dataKq.map(item => (
                                       <td key={item.provinceName + "2"} className={stylesCss['giai2']}>
-                                      {item[3].map(i => (
+                                      {item[3]?.map(i => (
                                         <div key={i.number}>{i.number}</div>
                                     ))}
                                  </td>
@@ -169,7 +169,7 @@ function BlockResultSX({title, data}) {
                                   <td className={stylesCss['giai1']}> Giải nhất</td>
                                     {dataKq.map(item => (
                                       <td key={item.provinceName + "1"} className={stylesCss['giai1']}>
-                                      {item[2].map(i => (
+                                      {item[2]?.map(i => (
                                         <div key={i.number}>{i.number}</div>
                                     ))}
                                  </td>
@@ -179,7 +179,7 @@ function BlockResultSX({title, data}) {
                                 <td className={stylesCss['giaidb']}> Giải đặc biệt</td>
                                 {dataKq.map(item => (
                                    <td key={item.provinceName + "db"} className={stylesCss['giaidb']}>
-                                   {item[1].map(i => (
+                                   {item[1]?.map(i => (
                                      <div key={i.number}>{i.number}</div>
                                    ))}
                                  </td>
